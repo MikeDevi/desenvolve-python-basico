8e6f4e26debfd31e64de71090d59a3aee68c4684
MikeDevi <mikael.tiagosbd@gmail.com>
2025-04-01 06:46:56 -0300

    ok

--- Modulo6/aula3_questao1.py
+++ Modulo6/aula3_questao1.py
@@ -1,9 +1,12 @@
 # 01
 lista = []
-num = 0
-while num == 1:
-    num = int(input('Digite um valor \nOu digite "1" para sair '))
+n = 0
+while n <=5:
+    num = int(input('Digite um valor \n'))
     lista.append(num)
+    n += 1
+    
 
 print(f'Valor original da lista: {lista}')
 print(f'3 Primeiros elementos: {lista[:3]}')
+print(f'3 Primeiros elementos: {lista[:-1]}')
